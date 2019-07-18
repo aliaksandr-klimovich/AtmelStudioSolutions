ea19306ea586f82e5c5e684ba7c5b69fbb069cc2
Aliaksandr Klimovich <aliaksandr_klimovich@icloud.com>
2019-07-19 00:04:55 +0300

    change tasking approach: now tasks are called from main

--- ATMEGA328P_PROJECTS/003_CLOCK_DISPLAY/main.c
+++ ATMEGA328P_PROJECTS/003_CLOCK_DISPLAY/main.c
@@ -17,7 +17,7 @@ void configure_timer1()
 {
     TCCR1B |= (1<< WGM12);  // set CTC mode
     OCR1AH = 0x02;
-    OCR1AL = 0x71;  // 625
+    OCR1AL = 0x71 - 3;  // 625 +/- adj.: (+) slower (-) faster
     TIMSK1 |= (1 << OCIE1A);
 }
 
@@ -29,13 +29,29 @@ void enable_timer1()
 
 #define TIMER1_TOP  (16000000UL / 1024UL / 625UL)
 
-uint8_t timer1_counter = 0;
-void task_200ms();
-void task_500ms();
+uint8_t timer1_counter;
+
 void task_1s();
-uint8_t task_1s_cycles = 0;
-uint8_t task_200ms_cycles = 0;
-uint8_t task_500ms_cycles = 0;
+void task_500ms();
+void task_200ms();
+
+uint8_t task_1s_counter;
+uint8_t task_500ms_counter;
+uint8_t task_200ms_counter;
+
+union TaskSwitches {
+    struct {
+        uint8_t task_1s_switch: 1;
+        uint8_t task_500ms_switch: 1;
+        uint8_t task_200ms_switch: 1;
+        uint8_t bit3: 1;
+        uint8_t bit4: 1;
+        uint8_t bit5: 1;
+        uint8_t bit6: 1;
+        uint8_t bit7: 1;
+    } b;
+    uint8_t u8;
+} task_switches;
 
 ISR(TIMER1_COMPA_vect)
 {
@@ -43,21 +59,50 @@ ISR(TIMER1_COMPA_vect)
 
     if (timer1_counter % (TIMER1_TOP / 5) == 0)
     {
-        task_200ms();
-        task_200ms_cycles++;
+        //task_200ms();
+        task_switches.b.task_200ms_switch = 1;
     }
-    
+
     if (timer1_counter % (TIMER1_TOP / 2) == 0)
     {
-        task_500ms();
-        task_500ms_cycles++;
+        //task_500ms();
+        task_switches.b.task_500ms_switch = 1;
     }
 
     if (timer1_counter == TIMER1_TOP)
     {
-        task_1s();
-        task_1s_cycles++;
+        //task_1s();
+        task_switches.b.task_1s_switch = 1;
         timer1_counter = 0;
+    }
+}
+
+void task_1s()
+{
+
+}
+
+uint8_t min;
+uint8_t sec;
+
+void task_500ms()
+{
+    if (task_500ms_counter % 2)
+    {
+        TM1637_print("%02u %02u", min, sec);
+
+        if (++sec >= 60)
+        {
+            sec = 0;
+            if (++min >= 60)
+            {
+                min = 0;
+            }
+        }
+    }
+    else
+    {
+        TM1637_print("%02u:%02u", min, sec);
     }
 }
 
@@ -66,37 +111,13 @@ void task_200ms()
 
 }
 
-uint8_t min = 0;
-uint8_t sec = 0;
-
-void task_500ms()
-{
-    if (task_500ms_cycles % 2)
-    {
-        TM1637_print("%02u %02u", min, sec);
-        
-        if (++sec >= 60)
-        {
-            sec = 0;
-            if (++min >= 60)
-            {
-                min = 0;
-            }
-        }  
-    }
-    else
-    {
-        TM1637_print("%02u:%02u", min, sec);
-    }     
-}
-
-void task_1s()
+ISR(INT0_vect)
 {
     
 }
 
 int main(void)
-{    
+{
     TM1637_init(&PC0, &PC1);
 
     sleep_enable();
@@ -104,36 +125,26 @@ int main(void)
     enable_timer1();
     sei();
 
-    /*
-    #include <avr/boot.h>
-    uint8_t low_fuses = boot_lock_fuse_bits_get(GET_LOW_FUSE_BITS);     // 255 = 1111_1111
-    uint8_t high_fuses = boot_lock_fuse_bits_get(GET_HIGH_FUSE_BITS);   // 218 = 1101_1010
-    TM1637_print("%u", high_fuses);
-
-    low_fuses
-        7 CKDIV8 = 1
-        6 CKOUT = 1
-        5 SUT1 = 1
-        4 SUT0 = 1
-        3 CKSEL3 = 1
-        2 CKSEL2 = 1
-        1 CKSEL1 = 1
-        0 CKSEL0 = 1
-
-    high_fuses
-        7 RSTDISBL = 1
-        6 DWEN = 1
-        5 SPIEN = 0
-        4 WDTON = 1
-        3 EESAVE = 1
-        2 BOOTSZ1 = 0
-        1 BOOTSZ0 = 1
-        0 BOOTRST = 0
-    */
-
     while (1)
     {
+        /*if (task_switches.b.task_1s_switch)
+        {
+            task_1s();
+            task_1s_counter++;
+            task_switches.b.task_1s_switch = 0;
+        }*/
+        if (task_switches.b.task_500ms_switch)
+        {
+            task_500ms();
+            task_500ms_counter++;
+            task_switches.b.task_500ms_switch = 0;
+        }
+        /*if (task_switches.b.task_200ms_switch)
+        {
+            task_200ms();
+            task_200ms_counter++;
+            task_switches.b.task_200ms_switch = 0;
+        }*/
         sleep_cpu();
     }
 }
-
